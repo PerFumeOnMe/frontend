32f05884d9d78ef7f2060365dc48bc9609352621
OhDongI <odi0716@naver.com>
2025-07-01 20:22:24 +0900

    ✨ feat: 카카오 로그인 이동 추가 (#1)

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -72,13 +72,13 @@ export default function LoginForm() {
 
         {/* 카카오 로그인 */}
         <div className="flex justify-center">
-          <button
-            type="button"
+          <Link
+            to="/kakao"
             className="flex items-center justify-center gap-[7px] w-96 h-[55px] bg-yellow-400 rounded-[30px] text-black font-bold cursor-pointer"
           >
             <img src="/Login/kakao.svg" alt="카카오톡" className="w-5 h-5" />
             카카오로 시작
-          </button>
+          </Link>
         </div>
 
         {/* 회원가입 안내 */}
---
+++ src/pages/Login/KakaoSignupPage.tsx
@@ -0,0 +1,9 @@
+import KakaoSignupForm from "../../components/Login/KakaoSignupForm"
+
+export default function KakaoSignupPage(){
+    return(
+        <div>
+            <KakaoSignupForm />
+        </div>
+    )
+}
