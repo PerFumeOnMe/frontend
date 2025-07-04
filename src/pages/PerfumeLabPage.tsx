4cc3ab5ed568337aee47f939b01c530af9decac6
chlwldms <132264125+chlwldms@users.noreply.github.com>
2025-07-04 19:03:25 +0900

    ✨ feat: 향수공방 view 틀 제작 (#3)

---
+++ src/pages/PerfumeLabPage.tsx
@@ -0,0 +1,70 @@
+import { SlArrowLeft } from "react-icons/sl";
+import { useNavigate } from "react-router-dom";
+
+const PerfumLabPage = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="min-w-[480px] bg-[#F4ECE6] flex flex-col items-center">
+      {/* 헤더 */}
+      <div className="w-full h-16 relative flex items-center border-b border-gray-300">
+        <button
+          onClick={() => navigate("/")}
+          className="absolute left-4 top-1/2 -translate-y-1/2"
+        >
+          <SlArrowLeft className="w-5 h-5" />
+        </button>
+        <h1 className="text-xl text-center w-full">향수공방</h1>
+      </div>
+      {/* 메인 콘텐츠 */}
+      <div className="w-full flex-1 flex flex-col gap-2">
+        {/* 인트로 텍스트 */}
+        <div className="w-full px-4 py-4 flex flex-col gap-1">
+          <h1 className="text-xl">이미지와 맞는 향수를 만들어봅시다.</h1>
+          <p className="text-xs text-gray-500">
+            향수를 노트로 나누는 건, 시간이 지날수록 향이 변해가는 이야기를 담기
+            위해서예요.
+          </p>
+        </div>
+        {/* 향수 */}
+        <div className="flex items-start justify-center">
+          <div className="w-full py-4 flex flex-row">
+            <div className="flex-[2] px-10 py-35 text-center border-dashed border-1 border-black">
+              향수 공병
+            </div>
+            <div className="flex-[3] px-10 py-35 text-center border-dashed border-1 border-black">
+              실린더 칸
+            </div>
+          </div>
+        </div>
+        {/* 내가 선택한 계열 */}
+        <div className="flex flex-1 flex-col justify-between">
+          <div className="flex justify-between items-center">
+            <h3 className="px-4 text-xl">내가 선택한 계열</h3>
+            <button className="border px-3 py-1 rounded-full text-xs mx-4">
+              초기화
+            </button>
+          </div>
+          {/* 노트 선택 칸 */}
+          <div className="flex flex-wrap justify-center gap-2 px-4 py-4 flex-col">
+            <button className="flex justify-center py-6 border-1 rounded border-[#FAFAFA] bg-white/40">
+              + 베이스 노트 선택하기
+            </button>
+            <button className="flex justify-center py-6 border-1 rounded border-[#FAFAFA] bg-white/40">
+              + 미들 노트 선택하기
+            </button>
+            <button className="flex justify-center py-6 border-1 rounded border-[#FAFAFA] bg-white/40">
+              + 탑 노트 선택하기
+            </button>
+          </div>
+          <div className="px-4 py-4">
+            <button className="w-full py-4 border rounded border-gray-300 text-gray-400 bg-white">
+              결과보기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PerfumLabPage;
