bd168e0cf02c4f3d585a01712f5a668e9cd77d7d
chlwldms <132264125+chlwldms@users.noreply.github.com>
2025-07-04 19:03:25 +0900

    ✨ feat: 향수공방 view 틀 제작 (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,51 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import NotFoundPage from './pages/NotFoundPage.tsx';
+import NotFoundPage from "./pages/NotFoundPage.tsx";
 import RootLayout from "./layout/root-layout.tsx";
-import MainPage from './pages/MainPage.tsx';
+import MainPage from "./pages/MainPage.tsx";
 import LoginPage from "./pages/Login/LoginPage.tsx";
-import SignupPage from './pages/Login/SignupPage.tsx';
-import MyPage from './pages/MyPage.tsx';
-import PBTIPage from './pages/PBTIPage.tsx';
-import DiaryPage from './pages/DiaryPage.tsx';
+import SignupPage from "./pages/Login/SignupPage.tsx";
+import MyPage from "./pages/MyPage.tsx";
+import PBTIPage from "./pages/PBTIPage.tsx";
+import DiaryPage from "./pages/DiaryPage.tsx";
 import ProtectedRoute from "./components/common/ProtectedRoute.tsx";
 import KakaoSignupPage from "./pages/Login/KakaoSignupPage.tsx";
+import PerfumLabPage from "./pages/PerfumeLabPage.tsx";
 
 const router = createBrowserRouter([
   // 로그인, 회원가입은 보호 라우트 없이 누구나 접근 가능
   {
     path: "/login",
-    element: <LoginPage />
+    element: <LoginPage />,
   },
   {
     path: "/signup",
-    element: <SignupPage />
+    element: <SignupPage />,
   },
   {
     path: "/kakao",
-    element: <KakaoSignupPage />
+    element: <KakaoSignupPage />,
   },
   // 아래는 보호 라우트로 감싼 실제 서비스 페이지들
   {
     element: <ProtectedRoute />, // 보호 라우트
     children: [
       {
-        path: '/',
+        path: "/",
         element: <RootLayout />,
         errorElement: <NotFoundPage />,
         children: [
           { index: true, element: <MainPage /> },
-          { path: 'PBTI', element: <PBTIPage /> },
-          { path: 'Diary', element: <DiaryPage /> },
-          { path: 'MyPage', element: <MyPage /> }
-        ]
-      }
-    ]
-  }
+          { path: "PBTI", element: <PBTIPage /> },
+          { path: "Diary", element: <DiaryPage /> },
+          { path: "MyPage", element: <MyPage /> },
+        ],
+      },
+      {
+        path: "/lab",
+        element: <PerfumLabPage />,
+      },
+    ],
+  },
 ]);
 
 function App() {
