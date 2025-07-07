444ee54ccc2d6063614d70ab8030674829c414dc
OhDongI <odi0716@naver.com>
2025-07-07 16:29:57 +0900

    💄 design: 카카오 로그인 버튼 색상 수정 (#1)

--- src/components/Login/SocialLogin.tsx
+++ src/components/Login/SocialLogin.tsx
@@ -5,7 +5,7 @@ export default function SocialLogin() {
     <div className="flex justify-center pt-[28px]">
       <Link
         to="/kakao"
-        className="flex items-center justify-center gap-[7px] w-96 h-[55px] bg-yellow-400 rounded-[30px] text-black font-bold"
+        className="flex items-center justify-center gap-[7px] w-96 h-[55px] bg-[#FEE500] rounded-[30px] text-black font-bold"
       >
         <img src="/Login/kakao.svg" alt="카카오톡" className="w-5 h-5" />
         카카오로 시작
