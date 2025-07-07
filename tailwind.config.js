6c1fca78eb660ebc1655afd90e1c7b02c7951e57
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2025-07-08 08:33:59 +0900

    💄 #8: font component 추가

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontWeight: {
+          light: 300,
+          regular: 400,
+          medium: 500,
+          semibold: 600,
+          bold: 700,
+        },
+      },
+    },
+    plugins: [],
+  }
