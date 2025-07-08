982fea9609df5d164094047fc5b95e47bf96c336
OhDongI <odi0716@naver.com>
2025-07-08 17:26:22 +0900

    💄 design: 헤더 영역 새로운 디자인 적용 (#1)

--- src/components/Signup/SignupHeader.tsx
+++ src/components/Signup/SignupHeader.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function SignupHeader() {
   return (
-    <header className="flex items-center justify-start ml-[35px] mt-[36px]">
+    <header className="flex items-center justify-start ml-[35px] mt-5">
       <Link to="/login">
-        <img src="/Login/return_shape.svg" alt="뒤로 가기" className="w-[11px] h-[18px]" />
+        <img src="/Login/return_shape.svg" alt="뒤로 가기" className="w-4 h-8" />
       </Link>
-      <h2 className="ml-[24px] text-xl font-semibold">로그인에 사용할 정보를 입력해주세요.</h2>
+      <h2 className="ml-[162px] text-title2 grayscale-900">회원가입</h2>
     </header>
   );
 }
