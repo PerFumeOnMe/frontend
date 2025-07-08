c7f27c9842d7f13ac405749cd8f08959e842830d
OhDongI <odi0716@naver.com>
2025-07-08 20:25:59 +0900

    💄 style: 수정 헤더 정렬 및 마크업 개선 (#1)

--- src/components/Signup/SignupFlow.tsx
+++ src/components/Signup/SignupFlow.tsx
@@ -12,7 +12,7 @@ export default function SignupFlow() {
 
   return (
     <main className="w-screen h-screen flex items-center justify-center">
-    <section className="w-full w-max-120 w-auto">
+    <section className="min-w-[375px] max-w-120 w-full mx-auto">
       {step === 0 && <SignupNameForm onNext={() => setStep(1)} />}
       {step === 1 && <SignupIdForm onNext={() => setStep(2)} />}
       {step === 2 && <SignupPasswordForm onNext={() => setStep(3)} />}
--- src/components/Signup/SignupHeader.tsx
+++ src/components/Signup/SignupHeader.tsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import returnIcon from "../../assets/Login/return_shape.svg";
 
 export default function SignupHeader() {
   return (
-    <header className="flex items-center justify-start ml-[35px] mt-5">
+    <header className="flex items-center justify-start ml-6 mt-5">
       <Link to="/login">
-        <img src="/Login/return_shape.svg" alt="뒤로 가기" className="w-4 h-8" />
+      <div className="w-4 h-8 mt-[1px]">
+        <img src={returnIcon} alt="뒤로 가기" className="ml-[1.34px] mt-[4.67px]" />
+      </div>  
       </Link>
-      <h2 className=" text-center flex-1 text-title2 grayscale-900">회원가입</h2>
-    </header>
+      <h2 className=" flex-1 text-title2 text-grayscale-900 mt-[1px] text-center">회원가입</h2>
+      <div className="w-4 h-8" />
+    </header> 
   );
 }
