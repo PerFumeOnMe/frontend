647efa9684f3e1c0dd4fcd97927ac36fb1898798
OhDongI <odi0716@naver.com>
2025-07-08 20:56:19 +0900

    💄 style: 타이틀 마크업 개선 (#1)

--- src/components/Signup/SignupTitle.tsx
+++ src/components/Signup/SignupTitle.tsx
@@ -1,9 +1,9 @@
 export default function SignupTitle({ title, subtitle }: { title: string; subtitle?: string }) {
   return (
     <div className="px-6 pt-8 gap-[6px]">
-      
+
       <h1 className="text-display1 whitespace-pre-line">{title}</h1>
-      {subtitle && <p className="text-body2 text-grayscale-600 mt-[6px]">{subtitle}</p>}
+      {subtitle && <p className="text-body2 text-grayscale-600 mt-[6px] mb-12">{subtitle}</p>}
     </div>
   );
 }
