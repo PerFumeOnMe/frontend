339889c6bba5ae3c8874532190b2ba9386a6aba2
OhDongI <odi0716@naver.com>
2025-07-08 23:39:46 +0900

    💄 style: 타이틀 스타일 개선 (#1)

--- src/components/Signup/SignupTitle.tsx
+++ src/components/Signup/SignupTitle.tsx
@@ -1,9 +1,8 @@
 export default function SignupTitle({ title, subtitle }: { title: string; subtitle?: string }) {
   return (
-    <div className="gap-[6px]">
-
-      <h1 className="text-display1 whitespace-pre-line">{title}</h1>
-      {subtitle && <p className="text-body2 text-grayscale-600 mt-[6px] mb-12">{subtitle}</p>}
+    <div className="max-w-[361px] w-full mx-auto flex flex-col gap-[6px]">
+      <h1 className="text-title2 whitespace-pre-line">{title}</h1>
+      {subtitle && <p className="text-body3 text-grayscale-600 mt-[6px] mb-12">{subtitle}</p>}
     </div>
   );
 }
