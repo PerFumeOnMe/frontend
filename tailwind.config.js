f3df9b1dd3f1bd1016aee7445fca54bf2efb0c40
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2025-07-08 09:30:05 +0900

    💄 #8: grayscale component 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        fontWeight: {
-          light: 300,
-          regular: 400,
-          medium: 500,
-          semibold: 600,
-          bold: 700,
-        },
-      },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        grayscale: {
+          1000: '#1C1C1E',
+          900: '#343437',
+          800: '#4C4C52',
+          700: '#65656C',
+          600: '#7E7E86',
+          500: '#98989F',
+          400: '#B2B2B8',
+          300: '#CDCDD0',
+          200: '#E7E7E9',
+        }
+      }
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
