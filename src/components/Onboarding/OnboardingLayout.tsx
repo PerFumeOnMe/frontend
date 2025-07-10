28f693e7a33970f1ba00a2b8a59d636b496b4edb
OhDongI <odi0716@naver.com>
2025-07-10 18:40:57 +0900

    💄 design: 프로그래스 크기 개선 (#15)

--- src/components/Onboarding/OnboardingLayout.tsx
+++ src/components/Onboarding/OnboardingLayout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function OnboardingLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full max-w-[480px] min-w-[375px] mx-autoflex flex-col justify-between min-h-screen bg-white">
+    <div className="w-full max-w-[480px] min-w-[375px] mx-auto flex flex-col justify-between min-h-screen bg-white">
       {children}
     </div>
   );
