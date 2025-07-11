5060ec9de1270ff2a86bbe2bfb2024eb0c22dc91
kim <jskim6335@naver.com>
2025-07-11 18:07:48 +0900

    :lipstick: 시작하기 버튼 디자인 완료

--- src/pages/PBTI/PBTIMainPage.tsx
+++ src/pages/PBTI/PBTIMainPage.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import PBTIHeader from '../../components/PBTI/PBTIHeader';
+import PBTIStartButton from '../../components/PBTI/PBTIStartButton';
 
 const PBTIMainPage: React.FC = () => {
 
   return (
     <div className="min-w-[375px] w-120 h-full bg-[#F8F0FF] flex flex-col items-center font-[Pretandard]">
       <PBTIHeader/>
-      <div className='w-[80%] border-1 border-[#65656C]'></div>
+      <div className='w-[88%] border-1 border-[#65656C]'></div>
+      <PBTIStartButton/>
     </div>
   );
 };
