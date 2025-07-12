a38e1f15af360fa29164bb58f8076ae879c15083
kim <jskim6335@naver.com>
2025-07-12 22:04:54 +0900

    :sparkles: PBTI 정보 소개 카드 추가

--- src/pages/PBTI/PBTIQuestionPage.tsx
+++ src/pages/PBTI/PBTIQuestionPage.tsx
@@ -1,10 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PBTIHeader from '../../components/PBTI/PBTIHeader';
+import PBTIStartButton from '../../components/PBTI/PBTIStartButton';
+import PBTIInfoCard from '../../components/PBTI/PBTIInfoCard';
 
 const PBTIQuestionPage: React.FC = () => {
+  const infoList = [
+    "질문 기반 해석 성향 키워드",
+    "성향 기반 향기 스타일",
+    "나에게 맞는 향수"
+  ];
 
   return (
-    <div className="min-w-[375px] w-120 h-full bg-[#F8F0FF] flex flex-col font-[Pretandard]">
-      
+    <div className="min-w-[375px] w-120 h-full bg-[#FBFBFB] flex flex-col items-center font-[Pretandard]">
+      <PBTIHeader />
+      <div className='w-[88%] border-1 border-[#65656C]'></div>
+      <div className='mt-6 text-[18px] text-grayscale-900 font-[600]'>
+        PBTI를 통해 확인할 수 있어요.
+      </div>
+      <div className='flex w-full justify-center m-6 gap-5'>
+        {infoList.map((info, idx) => (
+          <PBTIInfoCard key={idx} info={info} />
+        ))}
+      </div>
+      <PBTIStartButton />
     </div>
   );
 };
