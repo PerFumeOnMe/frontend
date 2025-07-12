4ab26dc3533cc2a3cf87fbb19169382336296675
kim <jskim6335@naver.com>
2025-07-12 22:04:54 +0900

    :sparkles: PBTI 정보 소개 카드 추가

--- src/components/PBTI/PBTIHeader.tsx
+++ src/components/PBTI/PBTIHeader.tsx
@@ -3,13 +3,12 @@ import character from "../../assets/PBTI/character.png"
 const PBTIHeader : React.FC = () => {
     return (
         <div className="flex flex-col items-center justify-center pt-30">
-            <div className="text-[32px] font-[700] text-main-500">
-                나의 향수 성향은?
+            <div className="text-[28px] font-semibold text-grayscale-1000">
+                나의 향수 성향 알아보기
             </div>
-            <div className="text-[14px] font-[500] text-main-500">
+            <div className="text-[16px] font-[400] text-grayscale-900">
                 성향으로 알아보는 향수 테스트
             </div>
-            <img src={character} className="my-12 w-[157px] h-[157px]" />
         </div>
     );
 }
