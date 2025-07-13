8a85a2b02e9335edd1fb6c98e2324b2a89c2c801
smileman62 <jskim6335@naver.com>
2025-07-13 20:59:41 +0900

    브랜치 내용과 맞지 않는 커밋 revert

--- src/components/PBTI/PBTIStartButton.tsx
+++ src/components/PBTI/PBTIStartButton.tsx
@@ -5,8 +5,8 @@ const PBTIStartButton : React.FC = () => {
     
     return (
         <button>
-            <div className="w-110 py-4 mt-5 mb-5 rounded-3xl bg-main-500 text-[#E7E7E9] text-[18px] font-[600]">
-                PBTI 시작하기
+            <div className="">
+                
             </div>
         </button>
     );
--- src/pages/PBTI/PBTIMainPage.tsx
+++ src/pages/PBTI/PBTIMainPage.tsx
@@ -1,32 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PBTIHeader from '../../components/PBTI/PBTIHeader';
 import PBTIStartButton from '../../components/PBTI/PBTIStartButton';
-import PBTIInfoCard from '../../components/PBTI/PBTIInfoCard';
 
 const PBTIMainPage: React.FC = () => {
-  const infoList = [
-    "나만의 성향 기반 향기 스타일을 \n 확인할 수 있어요.",
-    "질문 기반 해석 성향 키워드를 \n 확인할 수 있어요.",
-    "테스트를 통해 나에게 맞는 향수를 \n 추천 받을 수 있어요.",
-    "나에게 어울리는 향기 스타일 원형을 \n 찾을 수 있어요."
-  ];
 
   return (
-    <div className="min-w-[375px] w-120 h-screen bg-[#FBFBFB] flex flex-col items-center font-[Pretandard]">
-      <PBTIHeader />
-      <div className='flex flex-col mt-10 gap-4 w-[90%]'>
-        {infoList.map((info, idx) => (
-          <div
-            key={idx}
-            className={`flex ${idx % 2 === 0 ? 'justify-start' : 'justify-end'}`}
-          >
-            <PBTIInfoCard info={info}/>
-          </div>
-        ))}
-      </div>
-      <PBTIStartButton />
+    <div className="min-w-[375px] w-120 h-full bg-[#F8F0FF] flex flex-col items-center font-[Pretandard]">
+      <PBTIHeader/>
+      <div className='w-[88%] border-1 border-[#65656C]'></div>
+      <PBTIStartButton/>
     </div>
   );
 };
-
-export default PBTIMainPage;
