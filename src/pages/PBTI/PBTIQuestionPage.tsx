7f91c020332f958a73c344d8060d11a761fc31e0
smileman62 <jskim6335@naver.com>
2025-07-13 20:40:50 +0900

    Revert ":sparkles: PBTI 정보 소개 카드 추가"
    This reverts commit a38e1f15af360fa29164bb58f8076ae879c15083.
    
    브랜치와 맞지 않는 커밋 내용 revert

--- src/pages/PBTI/PBTIQuestionPage.tsx
+++ src/pages/PBTI/PBTIQuestionPage.tsx
@@ -1,28 +1,10 @@
-import React from 'react';
-import PBTIHeader from '../../components/PBTI/PBTIHeader';
-import PBTIStartButton from '../../components/PBTI/PBTIStartButton';
-import PBTIInfoCard from '../../components/PBTI/PBTIInfoCard';
+import React, { useState } from 'react';
 
 const PBTIQuestionPage: React.FC = () => {
-  const infoList = [
-    "질문 기반 해석 성향 키워드",
-    "성향 기반 향기 스타일",
-    "나에게 맞는 향수"
-  ];
 
   return (
-    <div className="min-w-[375px] w-120 h-full bg-[#FBFBFB] flex flex-col items-center font-[Pretandard]">
-      <PBTIHeader />
-      <div className='w-[88%] border-1 border-[#65656C]'></div>
-      <div className='mt-6 text-[18px] text-grayscale-900 font-[600]'>
-        PBTI를 통해 확인할 수 있어요.
-      </div>
-      <div className='flex w-full justify-center m-6 gap-5'>
-        {infoList.map((info, idx) => (
-          <PBTIInfoCard key={idx} info={info} />
-        ))}
-      </div>
-      <PBTIStartButton />
+    <div className="min-w-[375px] w-120 h-full bg-[#F8F0FF] flex flex-col font-[Pretandard]">
+      
     </div>
   );
 };
