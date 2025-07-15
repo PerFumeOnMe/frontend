84fa861dade2785666500736bbf115cbea0109bb
smileman62 <jskim6335@naver.com>
2025-07-15 22:51:58 +0900

    :lipstick: PBTI 메인페이지 시작 버튼 색 변화 애니메이션 추가. 커서 모양 변경. 너비 재조정.

--- src/components/PBTI/PBTIMain/PBTIStartButton.tsx
+++ src/components/PBTI/PBTIMain/PBTIStartButton.tsx
@@ -10,8 +10,13 @@ const PBTIStartButton : React.FC = () => {
     }
     
     return (
-        <button onClick={handleStart}>
-            <div className="w-80 py-3 mt-15 mb-5 rounded-3xl bg-main-500 text-title3 text-grayscale-200 font-[600]">
+        <button onClick={handleStart} className="w-full px-4 cursor-pointer">
+            <div className="
+                py-3 mt-15 mb-5 rounded-3xl 
+                bg-main-500 text-title3 text-grayscale-200 font-[600]
+                transition-colors duration-900 ease-in-out
+                hover:bg-[#490189]
+            ">
                 시작하기
             </div>
         </button>
