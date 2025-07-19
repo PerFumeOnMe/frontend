7ee2d0c3d4df781f494e3915dad9c13155058265
smileman62 <jskim6335@naver.com>
2025-07-20 02:55:26 +0900

    :lipstick: 결과 페이지 디자인 완성. 결과 저장 버튼의 모달 화면 구현. 모달의 애니메이션 구현

---
+++ src/components/PBTI/PBTIResult/PBTIRecommendedPerfume.tsx
@@ -0,0 +1,46 @@
+type PBTIRecommendedPerfumeProps = {
+  imageUrl: string;
+  brand: string;
+  name: string;
+  price: string;
+  description: string;
+};
+
+const PBTIRecommendedPerfume: React.FC<PBTIRecommendedPerfumeProps> = ({
+  imageUrl,
+  brand,
+  name,
+  price,
+  description,
+}) => {
+  return (
+    <div className="w-full h-full flex tracking-tighter">
+      {/* 이미지 파트 */}
+      <div className="relative w-21 h-25 flex-shrink-0">
+        <img src={imageUrl} alt={name} className="w-full h-full object-cover rounded-2xl shadow bg-amber-300" />
+        <div className="absolute bottom-0 right-1">
+          <span className="text-[18px]">💜</span>
+        </div>
+      </div>
+
+      {/* 텍스트 파트 */}
+      <div className="flex flex-col justify-start ml-4">
+        {/* 브랜드명과 가격 */}
+        <div className="flex flex-col">
+            <div className="flex justify-between items-center text-caption1 text-gray-800">
+                <span>{brand}</span>
+                <span className="text-body4 text-gray-700">{price}</span>
+            </div>
+
+            {/* 제품명 */}
+            <div className="text-body3 font-medium text-gray-1000 mb-1">{name}</div>
+        </div>
+
+        {/* 설명 */}
+        <div className="text-[12px] text-caption3 text-gray-800 mb-1">{description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default PBTIRecommendedPerfume;
