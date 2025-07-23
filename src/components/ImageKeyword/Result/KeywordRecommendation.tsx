45ae9ccc0150605a6149811f336194bac7c21b6a
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2025-07-24 03:00:52 +0900

    ✨ #33: button component

--- src/components/ImageKeyword/Result/KeywordRecommendation.tsx
+++ src/components/ImageKeyword/Result/KeywordRecommendation.tsx
@@ -1,37 +1,42 @@
+import { useNavigate } from 'react-router-dom';
 import type { RecommendedPerfume } from '../../../types/ImageKeyword/imageKeywordResult';
 import RecommendationCard from './RecommendationCard';
+import ResultButton from './ResultButton';
 
 interface KeywordRecommendationProps {
     recommendations: RecommendedPerfume[];
 }
 
 export default function KeywordRecommendation({ recommendations }: KeywordRecommendationProps) {
+    const navigate = useNavigate();
     return (
-        <div className="w-full">
-            <div className="w-full rounded-t-[24px] bg-[#FBFBFB]/40 border-[1px] border-white px-[25px] py-[16px]">
-                <h3 className="text-title2 text-grayscale-1000 text-center mb-[4px]">
-                    추천 향수
-                </h3>
-                <p className="text-title4 text-grayscale-900 text-center mb-[24px]">
-                    당신의 향기 무드에 가장 잘 어울리는 향수예요.
-                </p>
-                <div className="space-y-[36px]">
-                    {recommendations.map((perfume, index) => (
-                        <RecommendationCard
-                            key={index}
-                            brand={perfume.brand}
-                            name={perfume.name}
-                            notes={{
-                                topNote: perfume.topNote,
-                                middleNote: perfume.middleNote,
-                                baseNote: perfume.baseNote
-                            }}
-                            description={perfume.description}
-                            imageUrl={perfume.imageUrl || '/perfumes/default.png'}
-                            relatedKeywords={perfume.relatedKeywords}
-                        />
-                    ))}
-                </div>
+        <div className="w-full rounded-t-[24px] bg-[#FBFBFB]/40 border-[1px] border-white px-[25px] py-[16px]">
+            <h3 className="text-title2 text-grayscale-1000 text-center mb-[4px]">
+                추천 향수
+            </h3>
+            <p className="text-title4 text-grayscale-900 text-center mb-[24px]">
+                당신의 향기 무드에 가장 잘 어울리는 향수예요.
+            </p>
+            <div className="space-y-[36px]">
+                {recommendations.map((perfume, index) => (
+                    <RecommendationCard
+                        key={index}
+                        brand={perfume.brand}
+                        name={perfume.name}
+                        notes={{
+                            topNote: perfume.topNote,
+                            middleNote: perfume.middleNote,
+                            baseNote: perfume.baseNote
+                        }}
+                        description={perfume.description}
+                        imageUrl={perfume.imageUrl || '/perfumes/default.png'}
+                        relatedKeywords={perfume.relatedKeywords}
+                    />
+                ))}
+            </div>
+            <div className="flex justify-center gap-[16px] mt-[24px] mb-[16px]">
+                <ResultButton label="저장하기" onClick={() => alert('저장')} />
+                <ResultButton label="홈으로" onClick={() => navigate('/')} />
             </div>
         </div>
     );
---
+++ src/components/ImageKeyword/Result/ResultButton.tsx
@@ -0,0 +1,21 @@
+interface ResultButtonProps {
+    label: '저장하기' | '홈으로';
+    onClick: () => void;
+}
+
+export default function ResultButton({
+    label,
+    onClick
+}: ResultButtonProps) {
+    const bgClass = label === '저장하기' ? 'bg-main-400' : 'bg-main-500';
+    return (
+        <button
+            onClick={onClick}
+            className={`
+                flex-1 h-[48px] ${bgClass} text-title3 text-grayscale-200 rounded-[16px] flex items-center justify-center
+            `}
+        >
+            {label}
+        </button>
+    );
+} 
