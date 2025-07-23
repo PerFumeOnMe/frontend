deb408c818c0b120583d61a957a4b2eacb726a9d
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2025-07-24 03:32:07 +0900

    ✨ #33: save modal

--- src/components/ImageKeyword/Result/KeywordRecommendation.tsx
+++ src/components/ImageKeyword/Result/KeywordRecommendation.tsx
@@ -2,6 +2,9 @@ import { useNavigate } from 'react-router-dom';
 import type { RecommendedPerfume } from '../../../types/ImageKeyword/imageKeywordResult';
 import RecommendationCard from './RecommendationCard';
 import ResultButton from './ResultButton';
+import { useState } from 'react';
+import SaveCompleteModal from './SaveCompleteModal';
+import SaveNameModal from './SaveNameModal';
 
 interface KeywordRecommendationProps {
     recommendations: RecommendedPerfume[];
@@ -9,6 +12,10 @@ interface KeywordRecommendationProps {
 
 export default function KeywordRecommendation({ recommendations }: KeywordRecommendationProps) {
     const navigate = useNavigate();
+    const [showModal, setShowModal] = useState(false);
+    const [showCompleteModal, setShowCompleteModal] = useState(false);
+    const [perfumeName, setPerfumeName] = useState('');
+
     return (
         <div className="w-full rounded-t-[24px] bg-[#FBFBFB]/40 border-[1px] border-white px-[25px] py-[16px]">
             <h3 className="text-title2 text-grayscale-1000 text-center mb-[4px]">
@@ -35,9 +42,29 @@ export default function KeywordRecommendation({ recommendations }: KeywordRecomm
                 ))}
             </div>
             <div className="flex justify-center gap-[16px] mt-[24px] mb-[16px]">
-                <ResultButton label="저장하기" onClick={() => alert('저장')} />
+                <ResultButton label="저장하기" onClick={() => setShowModal(true)} />
                 <ResultButton label="홈으로" onClick={() => navigate('/')} />
             </div>
+
+            {showModal && (
+                <SaveNameModal
+                    onClose={() => setShowModal(false)}
+                    onSubmit={(name) => {
+                        setPerfumeName(name);
+                        setShowModal(false);
+                        setShowCompleteModal(true);
+                    }}
+                />
+            )}
+
+            {showCompleteModal && (
+                <SaveCompleteModal
+                    onConfirm={() => {
+                        setShowCompleteModal(false);
+                        navigate('/');
+                    }}
+                />
+            )}
         </div>
     );
 }
--- src/components/ImageKeyword/Result/ResultButton.tsx
+++ src/components/ImageKeyword/Result/ResultButton.tsx
@@ -1,16 +1,21 @@
 interface ResultButtonProps {
     label: '저장하기' | '홈으로';
     onClick: () => void;
+    disabled?: boolean;
 }
 
 export default function ResultButton({
     label,
-    onClick
+    onClick,
+    disabled = false
 }: ResultButtonProps) {
     const bgClass = label === '저장하기' ? 'bg-main-400' : 'bg-main-500';
+    const disabledClass = disabled ? 'opacity-50 cursor-not-allowed' : '';
+
     return (
         <button
             onClick={onClick}
+            disabled={disabled}
             className={`
                 flex-1 h-[48px] ${bgClass} text-title3 text-grayscale-200 rounded-[16px] flex items-center justify-center
             `}
