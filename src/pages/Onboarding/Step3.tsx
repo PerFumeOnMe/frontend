9fd39e3bc352a3722035595a4d9860fa1c556071
OhDongI <odi0716@naver.com>
2025-07-26 20:31:12 +0900

    💄 design: 토스트 위치 수정 (#15)

--- src/pages/Onboarding/Step3.tsx
+++ src/pages/Onboarding/Step3.tsx
@@ -10,31 +10,28 @@ import { AiOutlineExclamation } from "react-icons/ai";
 
 export default function OnboardingStep3({ onPrev }: { onPrev: () => void }) {
   const [selected, setSelected] = useState<string[]>([]);
-
-
-// 하드코딩? 이 부분 수정해야함. 토스트창 너비, 아이콘 위치
   const toggle = (id: string) => {
     if (selected.includes(id)) {
       setSelected(selected.filter((s) => s !== id));
     } else if (selected.length < 3) {
       setSelected([...selected, id]);
     } else {
-      toast.custom((t) => (
-     <div className="w-120 mx-4">  
-      <div
-        className={`${
-          t.visible ? "animate-enter" : "animate-leave"
-        } w-full mx-auto bg-grayscale-1000-f2 text-white rounded-2xl py-4 flex items-center gap-4.5`}
-      >
-        <div className="flex-shrink-0 w-5 h-5 rounded-full bg-white ml-7.5 flex items-center justify-center">
-          <AiOutlineExclamation className="w-4 h-4 text-grayscale-1000-f2" />
-        </div>
-        <div className="flex-1 text-center">
-          <span>선호하는 향은 3개까지만 설정이 가능합니다!</span>
+    toast.custom((t) => (
+      <div className="fixed bottom-20 w-full">
+        <div
+          className={`${t.visible ? "animate-enter" : "animate-leave"} mx-auto w-full max-w-[448px] bg-grayscale-1000-f2 text-white rounded-2xl py-4 px-5 flex items-center`}
+        >
+          <div className="flex items-center justify-center gap-3 w-full">
+            <div className="flex-shrink-0 w-5 h-5 rounded-full bg-white flex items-center justify-center">
+              <AiOutlineExclamation className="w-4 h-4 text-grayscale-1000-f2" />
+            </div>
+            <span className="text-center text-body3">
+              선호하는 향은 3개까지만 설정이 가능합니다.
+            </span>
+          </div>
         </div>
       </div>
-    </div> 
-));
+    ));
     }
   };
 
