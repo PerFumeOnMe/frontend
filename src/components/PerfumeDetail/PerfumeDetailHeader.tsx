ae6814b0d74cd630d01a4655fe081c630307cb1b
chlwldms <132264125+chlwldms@users.noreply.github.com>
2025-07-26 17:55:45 +0900

    ✨ feat: 뒤로가기 버튼 활성화 (#26)

--- src/components/PerfumeDetail/PerfumeDetailHeader.tsx
+++ src/components/PerfumeDetail/PerfumeDetailHeader.tsx
@@ -1,6 +1,13 @@
 import { SlArrowLeft } from "react-icons/sl";
+import { useNavigate } from "react-router-dom";
 
 const PerfumeDetailHeader = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="w-full relative">
       <img
@@ -8,7 +15,10 @@ const PerfumeDetailHeader = () => {
         alt="PerfumeImage"
         className="w-full h-100 object-cover rounded-b-3xl"
       />
-      <div className="absolute top-4 left-4 bg-black rounded-full p-3 shadow-md">
+      <div
+        className="absolute top-4 left-4 bg-black rounded-full p-3 shadow-md"
+        onClick={handleGoBack}
+      >
         <SlArrowLeft className="w-4 h-4 text-white" />
       </div>
     </div>
