212c16b8939558ef63c0da896be257bd7fe20761
chlwldms <132264125+chlwldms@users.noreply.github.com>
2025-07-31 16:18:52 +0900

    💄 design: 계열 레이어 이미지 추가 및 사이즈 조정

--- src/components/PerfumeLab/PerfumeBottle.tsx
+++ src/components/PerfumeLab/PerfumeBottle.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { usePerfumeLab } from "../../contexts/PerfumeLabContext";
 import { noteOptions } from "../../types/noteOptions";
 import type { Note } from "../../types/note";
+import BaseMask from "../../assets/PerfumeLab/masks/base_mask.png";
+import MiddleMask from "../../assets/PerfumeLab/masks/middle_mask.png";
+import TopMask from "../../assets/PerfumeLab/masks/top_mask.png";
 
 interface PerfumeBottleProps {
   perfumeImage: string;
@@ -25,73 +28,63 @@ const PerfumeBottle: React.FC<PerfumeBottleProps> = ({
 
   return (
     <div className={`relative ${className}`}>
-      <div className="relative w-full max-w-[200px] mx-auto">
+      <div className="relative w-full mx-auto">
+        {/* 향수병 이미지 */}
         <img
           src={perfumeImage}
-          className="w-full h-auto object-contain"
+          className="w-full h-auto object-contain relative z-10"
           alt="향수 공병"
         />
 
-        <div className="absolute inset-0 flex items-center justify-center z-0">
-          <svg
-            viewBox="0 0 120 150"
-            className="w-[100%] h-[100%]"
-            style={{ marginTop: "10%" }}
-          >
-            <defs>
-              <clipPath id="baseClip">
-                <path d="M25 115 L32 150 L88 150 L95 115 Z" />
-              </clipPath>
-              <clipPath id="middleClip">
-                <path d="M35 100 L85 100 L95 115 L25 115 Z" />
-              </clipPath>
-              <clipPath id="topClip">
-                <path d="M35 70 L35 100 L85 100 L85 70 L80 55 L40 55 Z" />
-              </clipPath>
-            </defs>
+        {/* 노트 레이어들 */}
+        <div className="absolute inset-0 flex flex-col justify-center">
+          {/* 베이스 노트 - 하단에 위치 */}
+          {hasBaseNote && (
+            <div
+              className="absolute inset-0"
+              style={{
+                backgroundImage: `url(${getNoteImage("베이스", hasBaseNote)})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center center",
+                WebkitMask: `url(${BaseMask}) no-repeat center bottom/93%`,
+                mask: `url(${BaseMask}) no-repeat center bottom/93%`,
+                bottom: "3%",
+                top: "40%",
+              }}
+            />
+          )}
 
-            {hasBaseNote && (
-              <g clipPath="url(#baseClip)">
-                <image
-                  href={getNoteImage("베이스", hasBaseNote)}
-                  x="25"
-                  y="110"
-                  width="70"
-                  height="30"
-                  preserveAspectRatio="xMidYMid slice"
-                  opacity="0.85"
-                />
-              </g>
-            )}
+          {/* 미들 노트 - 중간에 위치 */}
+          {hasMiddleNote && (
+            <div
+              className="absolute inset-0"
+              style={{
+                backgroundImage: `url(${getNoteImage("미들", hasMiddleNote)})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center center",
+                WebkitMask: `url(${MiddleMask}) no-repeat center/96%`,
+                mask: `url(${MiddleMask}) no-repeat center/96%`,
+                top: "43%",
+                bottom: "4%",
+              }}
+            />
+          )}
 
-            {hasMiddleNote && (
-              <g clipPath="url(#middleClip)">
-                <image
-                  href={getNoteImage("미들", hasMiddleNote)}
-                  x="30"
-                  y="85"
-                  width="70"
-                  height="30"
-                  preserveAspectRatio="xMidYMid slice"
-                  opacity="0.85"
-                />
-              </g>
-            )}
-
-            {hasTopNote && (
-              <g clipPath="url(#topClip)">
-                <image
-                  href={getNoteImage("탑", hasTopNote)}
-                  x="35"
-                  y="55"
-                  width="50"
-                  height="45"
-                  preserveAspectRatio="xMidYMid slice"
-                  opacity="0.85"
-                />
-              </g>
-            )}
-          </svg>
+          {/* 탑 노트 - 상단에 위치 */}
+          {hasTopNote && (
+            <div
+              className="absolute inset-0"
+              style={{
+                backgroundImage: `url(${getNoteImage("탑", hasTopNote)})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center center",
+                WebkitMask: `url(${TopMask}) no-repeat center top/79%`,
+                mask: `url(${TopMask}) no-repeat center top/79%`,
+                top: "44%",
+                bottom: "20%",
+              }}
+            />
+          )}
         </div>
       </div>
     </div>
