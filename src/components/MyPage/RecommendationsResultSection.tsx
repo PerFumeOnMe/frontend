73b9d538de4e151a3e319e886be8d4a0a7a1ade0
kim <jskim6335@naver.com>
2025-07-31 22:34:30 +0900

    작업 수정

--- src/components/MyPage/RecommendationsResultSection.tsx
+++ src/components/MyPage/RecommendationsResultSection.tsx
@@ -1,16 +1,20 @@
 import MyPageRecommendationCard from "./MyPageRecommendationCard";
+import SkeletonRecommendationCard from "./SkeletonRecommendationCard";
 
+const RecommendationsResultSection: React.FC<{ isLoading?: boolean }> = ({ isLoading = false }) => {
+  return (
+    <div className="flex flex-col w-full">
+      {isLoading
+        ? Array.from({ length: 3 }).map((_, idx) => <SkeletonRecommendationCard key={idx} />)
+        : <>
+            <MyPageRecommendationCard />
+            <MyPageRecommendationCard />
+            <MyPageRecommendationCard />
+          </>
+      }
+    </div>
+  );
+};
 
-const RecommendationsResultSection : React.FC = () => {
-    
-    
-    return (
-        <div className="flex flex-col w-full">
-            <MyPageRecommendationCard/>
-            <MyPageRecommendationCard/>
-            <MyPageRecommendationCard/>
-        </div>
-    );
-}
 
 export default RecommendationsResultSection;
