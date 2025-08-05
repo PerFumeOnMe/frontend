c00c9efd01449174f39e143d9a50656d310c96f8
chlwldms <132264125+chlwldms@users.noreply.github.com>
2025-08-06 01:15:47 +0900

    ✨ feat: context 및 hook을 이용하여 api 연결 (#61)

--- src/components/PerfumeDetail/PerfumeDescription.tsx
+++ src/components/PerfumeDetail/PerfumeDescription.tsx
@@ -1,9 +1,12 @@
+import { usePerfumeExtraInfo } from "../../hooks/usePerfumeDetail";
 import DescriptionNote from "./DescriptionNote";
 import DiffusionInfo from "./DiffusionInfo";
 import ExtraInfo from "./ExtraInfo";
 import LastingInfo from "./LastingInfo";
 
 const PerfumeDescription = () => {
+  const { lastingPower, diffusionRange, gender, locations, seasons } =
+    usePerfumeExtraInfo();
   return (
     <div className="w-full min-h-screen flex flex-col gap-12 bg-[#FBFBFB]/20 border border-white shadow-[0_-10px_10px_-3px_rgba(0,0,0,0.05),0_10px_10px_-3px_rgba(0,0,0,0.05)] pt-5 pb-20 pl-8 pr-8">
       <div className=" flex flex-col justify-center items-center gap-2">
@@ -13,15 +16,14 @@ const PerfumeDescription = () => {
             퍼퓨온미가 쉽게 설명해 줄게요.
           </p>
         </div>
-        {/* 전체적인 퍼블리싱 끝나고 애니메이션 구현 */}
         <DescriptionNote />
       </div>
-      <LastingInfo lastingData="1~2시간" />
-      <DiffusionInfo activeDiffusion={3} />
+      <LastingInfo lastingData={lastingPower || "1~2시간"} />
+      <DiffusionInfo activeDiffusion={diffusionRange || 3} />
       <ExtraInfo
-        gender="여성용"
-        locations={["일상용", "데이트/로맨틱"]}
-        seasons={["봄", "여름"]}
+        gender={gender || "남녀공용"}
+        locations={locations || []}
+        seasons={seasons || []}
       />
     </div>
   );
