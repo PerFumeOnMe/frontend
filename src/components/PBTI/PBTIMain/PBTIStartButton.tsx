a0364e47cc618b1124df6d36a9e64b04e87b8547
smileman62 <jskim6335@naver.com>
2025-08-21 23:18:06 +0900

    :lipstick: 메인페이지 디자인 수정. 이제 PBTI gif이 같이 움직임

--- src/components/PBTI/PBTIMain/PBTIStartButton.tsx
+++ src/components/PBTI/PBTIMain/PBTIStartButton.tsx
@@ -12,7 +12,7 @@ const PBTIStartButton : React.FC = () => {
     return (
         <button onClick={handleStart} className="w-full px-4 cursor-pointer">
             <div className="
-                py-3 mt-15 mb-5 rounded-3xl 
+                py-3 mt-10 mb-10 rounded-3xl 
                 bg-main-500 text-title3 text-grayscale-200 font-[600]
                 transition-colors duration-900 ease-in-out
                 hover:bg-[#490189]
